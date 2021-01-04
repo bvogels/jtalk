ba796dff9f4a86e8f540776a12a009aac18f60d8
bvogels <benjamin.vogels@gmx.de>
2021-01-04 11:13:34 +0100

    Worked on the userProfile.java file

--- src/main/java/at/jtalk/gui/userProfile.java
+++ src/main/java/at/jtalk/gui/userProfile.java
@@ -1,13 +1,17 @@
 package at.jtalk.gui;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,6 +22,12 @@ public class userProfile implements Initializable{
     private javafx.scene.control.Button exit;
     @FXML
     private javafx.scene.control.Button save;
+    @FXML
+    TextField signUpUser;
+    @FXML
+    TextField signUpPassword;
+    @FXML
+    TextField ipAddressField;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -33,9 +43,35 @@ public class userProfile implements Initializable{
         stage.show();
     }
 
-    public void saveUserProfile(ActionEvent actionEvent) throws IOException{
+    public void saveUserProfile(ActionEvent actionEvent) throws IOException {
+        // obtaining the user details
+        String user = signUpUser.getText();
+        String password = signUpPassword.getText();
+        String ipaddress = ipAddressField.getText();
 
+        // When save button is clicked, the details are saved
+        EventHandler<ActionEvent> saveProfile = event -> {
+            try {
+                File users = new File("users.txt");
+                if (users.createNewFile()) {
+                    System.out.println("Created new file of user names with name users.txt");
+                } else {
+                    System.out.println("File already exists. Appending.");
+                }
+            } catch (IOException error) {
+                System.out.println("Bad luck. File could not be created.");
+                error.printStackTrace();
+            }
 
-
+            try {
+                FileWriter saveUserDetails = new FileWriter("users.txt");
+                saveUserDetails.write(user + password + ipaddress);
+                saveUserDetails.close();
+            } catch (IOException error) {
+                System.out.println("Could not save data.");
+            }
+        };
+        save.setOnAction(saveProfile);
     }
 }
+
